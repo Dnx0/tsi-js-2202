2ed952dbe1d050dd5eae4962cdf4279b0b0b138d
Victor Pereira <82619763+Dnx0@users.noreply.github.com>
2022-11-23 16:00:05 -0300

    aula final

---
+++ calculaServico.js
@@ -0,0 +1,31 @@
+/*
+Criar uma calculadora de valor a ser 
+cobrado do cliente.
+O valor do serviço pode variar conforme o 
+prazo.
+Os valores e prazos devem ser armazenados 
+em uma matriz.
+O cálculo deve ficar obrigatoriamente em 
+uma funçãoe
+*/
+
+let tabela = [  ['$$$','$$','$'],
+                ['$$$$','$$$','$$'],
+                ['Não faz','Não faz','$$$']];
+
+let servico = prompt("Entre com o serviço: \n0 - Troca de óleo \n1 - Balanceamento\n 2 - Cambagem ");
+let prazo = prompt("Entre com o prazo: \n0 - Um dia \n1 - Dois dias\n 2 - Três dias ");
+
+if( validaEntrada(servico) && validaEntrada(prazo) ){
+
+    alert("O serviço ficará em: " + tabela[servico][prazo]);
+
+}else{
+
+    alert("ERRO: Verifique os valores inseridos e tente novamente!");
+}
+
+function validaEntrada(dadoUsuario){
+
+    return dadoUsuario >= 0 && dadoUsuario <= 2 ? true : false;
+}
---
+++ storage/sessionStorage.js
@@ -0,0 +1,62 @@
+//Como trabalhar com SESSION STORAGE
+
+let btn = document.getElementById('botao');
+
+btn.addEventListener('click', mostrar);
+
+function mostrar(evento){
+    evento.preventDefault();
+
+    let entrada = document.getElementById('entrada');
+    let mostrar = document.getElementById('mostrar');
+    let valor = entrada.value;
+    mostrar.innerText = valor;
+    entrada.value = '';
+
+    //após obtermos o dado do usuário
+    //vamos guardá-lo no local storage
+    sessionStorage.setItem('dadoDoUsuario', valor);
+}
+
+//Para recuperar o dado de local storage
+//usamo o sessionStorage.getItem()
+document.querySelector('#mostrar').innerText =
+    sessionStorage.getItem('dadoDoUsuario');
+
+//E para apagar no session storage?
+//removeItem()
+//sessionStorage.removeItem('dadoDoUsuario');   
+
+let vetor = ['pera','banana','manga'];
+
+//Para gardar algo que não seja uma string
+//no localStorage ou sessionStorage, temos
+//que converter para string. Com o método
+//JSON.stringify() transformamos vetores
+//e objetos em string
+sessionStorage.setItem( 'frutas', 
+                        JSON.stringify(vetor));
+
+//Para recuperar uma string JSON do localStorage
+//ou sessionStorage e transformar em um objeto
+//ou vetor novamente, use JSON.parse()
+let recupDado = JSON.parse(
+                    sessionStorage.
+                        getItem('frutas'));
+
+//Alterações no vetor recuperado 
+//do sessionStorage
+recupDado[3] = 'morango';
+recupDado[1] = 'parchita';
+
+//console.log(recupDado);
+
+//Transforma o vetor em string para poder
+//gravar em sessionStorage ou localStorage
+let strRecupDados = JSON.stringify(recupDado);
+
+//console.log(strRecupDados);
+
+//Atualiza o item frutas no sessionStorage
+sessionStorage.setItem('frutas', strRecupDados );
+
